1d4cd9ad76895537570b81787c9066f009ac9aac
Arno Dirlam <arnodirlam@googlemail.com>
2017-05-07 19:11:38 +0200

    Allocation: Implement feasible? and complete?

--- lib/allocation.rb
+++ lib/allocation.rb
@@ -1,21 +1,41 @@
+require 'constraints'
+
 # Represents a (partial) allocation of meetings to meeting room time slots
 # It does not necessarily need to be feasible or complete (see `feasible?` and `complete?`, respectively)
 class Allocation
   def initialize(constraints, allocation)
     @constraints = constraints
-    @allocation = allocation
+    @allocation = allocation  # Array of Hashes
   end
 
   # Checks whether the allocation is feasible,
   # i.e. whether any meeting room time slot is over capacity
   def feasible?
-    nil
+    @constraints.time_slots.each_with_index do |max_duration,i|
+      sum = 0
+      @allocation[i].each do |k,v|
+        sum += k * v
+        return false if sum > max_duration
+      end
+    end
+    true
   end
 
   # Checks whether the allocation is complete,
   # i.e. whether all meetings are allocated
   def complete?
-    nil
+    remaining_meetings.empty?
+  end
+
+  # Returns how many meetings of each duration are not allocated
+  def remaining_meetings
+    remaining_meetings = @constraints.meetings.dup
+    @allocation.each do |time_slot_hash|
+      time_slot_hash.each do |k,v|
+        remaining_meetings[k] -= v
+      end
+    end
+    remaining_meetings.reject{ |k,v| v <= 0 }
   end
 
   # Return all sub-nodes in the tree of possible allocations (see `README`)
--- spec/allocation_spec.rb
+++ spec/allocation_spec.rb
@@ -3,6 +3,20 @@ require 'allocation'
 describe Allocation do
   let(:constraints) { Constraints.new }
 
+  context "empty allocation" do
+    let(:allocation) do
+      Allocation.new(constraints, [{}, {}, {}, {}])
+    end
+
+    it "is feasible" do
+      expect(allocation).to be_feasible
+    end
+
+    it "is not complete" do
+      expect(allocation).not_to be_complete
+    end
+  end
+
   context "test allocation" do
     let(:allocation) do
       test_allocation = [
@@ -16,6 +30,26 @@ describe Allocation do
 
     it "is feasible" do
       expect(allocation).to be_feasible
+    end
+
+    it "is complete" do
+      expect(allocation).to be_complete
+    end
+  end
+
+  context "overbooked allocation" do
+    let(:allocation) do
+      test_allocation = [
+        {30 => 1, 45 => 2, 60 => 1},
+        {30 => 1, 60 => 4},
+        {30 => 3, 45 => 2, 60 => 1},
+        {30 => 1, 45 => 2}
+      ]
+      Allocation.new(constraints, test_allocation)
+    end
+
+    it "is not feasible" do
+      expect(allocation).not_to be_feasible
     end
 
     it "is complete" do
